37d54179f3481fcb703717b65d80fde35ce24227
Elijah Kurganskii <ilya.kurganskii@gmail.com>
2020-12-13 13:03:35 +0400

    Feature/#8 prettier (#9)
    * #8: replace tslint to eslint
    
    * #8: add linter for tests
    
    * #8: add prettier
    
    * #8: add prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {PrimeNGConfig} from "primeng/api";
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   constructor(private primengConfig: PrimeNGConfig) {}
 
   ngOnInit() {
